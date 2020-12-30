a2333c64ff150938b1e0c2ea7885a3b5d0ffdd8f
Miguel <miguel.moreno@dataleanmakers.es>
2020-12-30 19:01:27 +0100

    migraciones

---
+++ database/migrations/2020_12_30_175556_create_imagenes_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateImagenesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('imagenes', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreign("articuloId")->references("id")->on("articulos");
+            $table->string("urlAmazon");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('imagenes');
+    }
+}
